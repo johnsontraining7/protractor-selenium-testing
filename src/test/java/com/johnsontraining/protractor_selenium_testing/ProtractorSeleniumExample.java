b93751047d96ad662dd86e27b3f320fb780393d3
ezjohnson <ezjohnson@conviva.com>
2020-08-01 09:12:02 +0530

    First commit to protractor-selenium-project which contains jprotractor jar

---
+++ src/test/java/com/johnsontraining/protractor_selenium_testing/ProtractorSeleniumExample.java
@@ -0,0 +1,11 @@
+package com.johnsontraining.protractor_selenium_testing;
+
+import org.testng.annotations.Test;
+
+public class ProtractorSeleniumExample {
+
+	@Test
+	public void loginAsCustomerTest() {
+		
+	}
+}
